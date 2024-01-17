285f1b251f2fa448c98d0bde2f460a8318af1c1b
Vladislav Mishurin <mshrvl17@gmail.com>
2024-01-17 11:54:59 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+
+
+fun main() {
+val result = checkPrice(1000,5,"Silver",true)
+}
+
+fun checkPrice(price: Int, discount: Int, level: String, newUser: Boolean) {
+    var totalPrice = price - discount
+
+    val cashback = when (level) {
+        "Gold" -> 20
+        "Silver" -> 10
+        "Bronze" -> 5
+        else -> 0
+    }
+
+    val totalPointsForOrder = if(newUser) {
+        totalPrice / 100 * cashback + 500
+    } else {
+        totalPrice / 100 * cashback
+    }
+    println("Сумма кешбека составит $totalPointsForOrder")
+    println("Сумма заказа $totalPrice")
+
+
+}
+
