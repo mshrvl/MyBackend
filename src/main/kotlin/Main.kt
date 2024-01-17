5317eae0f2611a079fffa8349c38e403d94e60a0
Vladislav Mishurin <mshrvl17@gmail.com>
2024-01-17 11:55:20 +0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ val result = checkPrice(1000,5,"Silver",true)
 fun checkPrice(price: Int, discount: Int, level: String, newUser: Boolean) {
     var totalPrice = price - discount
 
-    val cashback = when (level) {
+    var cashback = when (level) {
         "Gold" -> 20
         "Silver" -> 10
         "Bronze" -> 5
